4619d8c0495eefef2e2347cf059a4748be447415
IvanNavas092 <navass21112@gmail.com>
2025-05-25 01:52:04 +0200

    remove ensure for login

--- skillping_server/marketplace/views_api_view.py
+++ skillping_server/marketplace/views_api_view.py
@@ -45,7 +45,6 @@ def get_csrf_token(request):
 # -----------------
 @api_view(["POST"])
 @permission_classes([AllowAny])
-@ensure_csrf_cookie
 def login_view(request):
     username = request.data.get("username")
     password = request.data.get("password")
