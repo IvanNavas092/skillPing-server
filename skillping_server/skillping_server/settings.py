7f882e73d2bd100e1f2831381fd0c0ab21905614
IvanNavas092 <navass21112@gmail.com>
2025-05-25 11:38:34 +0200

    fix error of android

--- skillping_server/skillping_server/settings.py
+++ skillping_server/skillping_server/settings.py
@@ -70,6 +70,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 AUTHENTICATION_BACKENDS = [
@@ -174,7 +177,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
