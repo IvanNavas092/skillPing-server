72db79763f740022b6ba3738c92c60efc85e3eb8
navas <98425766+IvanNavas092@users.noreply.github.com>
2025-05-25 13:00:56 +0200

    Update settings.py

--- skillping_server/skillping_server/settings.py
+++ skillping_server/skillping_server/settings.py
@@ -101,17 +101,13 @@ CORS_ALLOWED_ORIGINS = [
     # localhost http or https
     "http://localhost",
     "https://localhost",
-    # capacitor
-    "capacitor://localhost",
-    # ionic
-    "ionic://localhost",
     # netlify
     "skillping.netlify.app",
 ]
 # CSRF trusted origin for avoiding CSRF errors
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:4200",
-    # "https://skillping.netlify.app",
+    "https://skillping.netlify.app",
     "http://localhost",
 ]
 
